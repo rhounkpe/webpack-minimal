d580f9d47a6748f568569a867a5bfc0cd608bcbd
Rufin Hounkpe <rhounkpe@gmail.com>
2019-06-10 14:41:33 +0200

    Loading images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+import Toucan from './asserts/images/Toucan.jpg';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    // Add the image to our existing div.
+    const toucan = new Image();
+    toucan.src = Toucan
+
+    element.appendChild(toucan);
 
     return element;
 }
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+};
